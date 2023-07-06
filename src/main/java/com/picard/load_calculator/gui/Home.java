7901f0db4a8ae86b4f92c1c9d8e6b00667537324
Valentin <valentinpicard.tech@gmail.com>
2023-07-06 14:03:44 +0200

    handle temps réel

--- src/main/java/com/picard/load_calculator/gui/Home.java
+++ src/main/java/com/picard/load_calculator/gui/Home.java
@@ -19,6 +19,12 @@ import java.util.List;
 
 @Slf4j
 public class Home extends JFrame {
+    private int i = 0;
+
+    private Container cp = this.getContentPane();
+    private ActivityController activityController;
+    private FosterController fosterController;
+    private JButton addActivityButton = new JButton("Ajouter une activité");
     public Home(
             ActivityController activityController,
             FosterController fosterController
@@ -28,40 +34,11 @@ public class Home extends JFrame {
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         setTitle(getClass().getSimpleName());
         setSize(800, 600);
+        this.fosterController = fosterController;
+        this.activityController = activityController;
+        this.i = 0;
 
-        Container cp = this.getContentPane();
-
-        SwingUtilities.invokeLater(new Runnable() {
-               @Override
-               public void run() {
-
-                   Foster foster = fosterController.getTrainningState(LocalDate.now());
-                   TrainningState trainningState = fosterController.calculateTrainningState(foster);
-
-                   TrainningStateBanner trainningStateBanner = new TrainningStateBanner(
-                           foster, trainningState
-                   );
-                   cp.add(trainningStateBanner.getRootPane(), BorderLayout.NORTH);
-
-                   JTable table = new JTable();
-                   List<Activity> activityList = activityController.getAllActivities();
-
-                   String[] columnNames = {"Nom", "Date", "Charge"};
-                   DefaultTableModel model = new DefaultTableModel(columnNames, 0);
-                   for (Activity activity : activityList) {
-                       String name = activity.getName();
-                       LocalDate date = activity.getDate();
-                       int load = activity.getLoad();
-                       model.addRow(new Object[]{name, date, load});
-                   }
-                   new GridLayout(3, 4);
-                   table.setModel(model);
-                   cp.add(new JScrollPane(table));
-               }
-           });
-
-        JButton addActivityButton = new JButton("Ajouter une activité");
-        cp.add(addActivityButton, BorderLayout.SOUTH);
+        SwingUtilities.invokeLater(updateView);
 
         addActivityButton.addActionListener(new ActionListener() {
             @Override
@@ -76,16 +53,49 @@ public class Home extends JFrame {
                         dialog.addWindowListener(new WindowAdapter() {
                             @Override
                             public void windowClosed(WindowEvent e) {
-                                cp.repaint();
-                                cp.revalidate();
+                                i++;
+                                cp.removeAll();
+                                SwingUtilities.invokeLater(updateView);
                             }
                         });
                     }
                 });
             }
         });
-        cp.revalidate();
     }
+
+    private Runnable updateView = new Runnable() {
+
+        @Override
+        public void run() {
+            Foster foster = fosterController.getTrainningState(LocalDate.now());
+            TrainningState trainningState = fosterController.calculateTrainningState(foster);
+
+            TrainningStateBanner trainningStateBanner = new TrainningStateBanner(
+                    foster, trainningState
+            );
+            cp.add(trainningStateBanner.getRootPane(), BorderLayout.NORTH);
+
+            JTable table = new JTable();
+            List<Activity> activityList = activityController.getAllActivities();
+
+            String[] columnNames = {"Nom", "Date", "Charge"};
+            DefaultTableModel model = new DefaultTableModel(columnNames, 0);
+            for (Activity activity : activityList) {
+                String name = activity.getName();
+                LocalDate date = activity.getDate();
+                int load = activity.getLoad();
+                model.addRow(new Object[]{name, date, load});
+            }
+            new GridLayout(3, 4);
+            table.setModel(model);
+            table.setEnabled(false);
+            cp.add(new JScrollPane(table));
+            cp.add(addActivityButton, BorderLayout.SOUTH);
+            cp.repaint();
+            cp.revalidate();
+        }
+    };
 
     @Override
     public void dispose() {
